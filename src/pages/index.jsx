625096f19e5a7d450cfdb978eaa1b9c6fa816865
André Leite <andreleite1184@hotmail.com>
2021-07-11 14:18:33 -0300

    att

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function inicio(){
+    return (
+        <div>
+            <h1>Fundamentos de Next.js & React</h1>
+            <h2>Vamos estudar esse Framework</h2>
+        </div>
+    )
+}
+
